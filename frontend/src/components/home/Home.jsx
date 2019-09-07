b3a567f0d51b941abc84c6fa343ebfa4228ebcf7
Alan Vieceli <alansvieceli@gmail.com>
2019-09-07 17:01:43 -0300

    Componente Conteúdo

---
+++ frontend/src/components/home/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Main from '../template/Main';
+
+export default props =>
+    <Main icon="home" titile="Início"
+        subtitle="Projeto CRUD Básico com React.">
+        <div className="display-4">Olá!!!</div>
+        <p className="mb-0">Sistema de CRUD simples....</p>
+    </Main>
--- frontend/src/main/App.jsx
+++ frontend/src/main/App.jsx
@@ -7,12 +7,12 @@ import React from 'react'
 import Logo from '../components/template/logo/Logo'
 import Nav from '../components/template/nav/Nav'
 import Footer from '../components/template/footer/Footer'
-import Main from '../components/template/Main'
+import Home from '../components/home/Home'
 
 export default props =>
     <div className="app">
         <Logo />
         <Nav />
-        <Main icon="home" titile="Início" subtitle="Projeto CRUD Básico com React."/>
+        <Home />
         <Footer />
     </div>
