679631738b3a35dbd108423bd29aef5da6a2d47a
Alan Vieceli <alansvieceli@gmail.com>
2019-09-08 12:53:29 -0300

    Parte funcional do Cadastro

--- frontend/src/components/user/UserCRUD.jsx
+++ frontend/src/components/user/UserCRUD.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
+import axios from 'axios'
+
 import Main from '../template/Main'
+import Constants from '../../utils/Constants'
 
 const headerProps = {
     icon: 'users',
@@ -7,7 +10,38 @@ const headerProps = {
     subtitle: "Cadastro de usuários: Incluir, Listar, Alterar e Excluir"
 }
 
+//estado inicial
+const initialState = {
+    usar: { name: '', email: '' },
+    list: []
+}
+
 export default class UserCrud extends React.Component {
+
+    state = { ...initialState }
+
+    getUpdatedList(user){
+        const list = this.state.list.filter(u => u.id !== user.id);
+        list.unshift(user) //coloca elemento na primeira posição do array
+        return list;
+    }
+
+    clear() {
+        this.setState({ user: initialState.user })
+    }
+
+    save() {
+        const user = this.state.user;
+        const method = user.id ? 'put' : 'post'
+        const url = user.id ? `${Constants.BASE_URL}/${user.id}` : Constants.BASE_URL
+    
+        //axios.post ou axios.put
+        axios[method](url, user)
+            .then(resp => {
+                const list = this.getUpdatedList(resp.data)
+                this.setState({ user: initialState.user, list})
+            })    
+    }
 
     render() {
         return (
---
+++ frontend/src/utils/Constants.js
@@ -0,0 +1,7 @@
+const Constants = {
+    BASE_URL: 'http://localhost:3001/user'
+}
+
+Object.freeze(Constants);
+
+export default Constants
